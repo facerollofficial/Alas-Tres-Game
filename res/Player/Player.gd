c205fc98614d9f2543411cac5189a4e8960d2675
facerollofficial <82850357+facerollofficial@users.noreply.github.com>
2022-09-08 13:44:52 +0800

    rsc_6

--- res/Player/Player.gd
+++ res/Player/Player.gd
@@ -6,6 +6,11 @@ const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+
+onready var animationState = animationTree.get("parameters/playback")
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -13,22 +18,15 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		if input_vector.y > 0:
-			$AnimatedSprite.play("down")
-		elif input_vector.y <0:
-			$AnimatedSprite.play("up")
-		else:
-			if input_vector.x > 0:
-				$AnimatedSprite.flip_h = false
-				$AnimatedSprite.play("right")
-			elif input_vector.x < 0:
-				$AnimatedSprite.flip_h = true
-				$AnimatedSprite.play("right")
+	
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		$AnimatedSprite.stop()
-		$AnimatedSprite.frame = 0
 	
 	velocity = move_and_slide(velocity)
