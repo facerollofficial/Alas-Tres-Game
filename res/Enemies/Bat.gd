6b9bc99eb8e97ee493e82d01b2e9ac635213cf39
facerollofficial <82850357+facerollofficial@users.noreply.github.com>
2022-09-22 15:04:35 +0800

    rsc_13

--- res/Enemies/Bat.gd
+++ res/Enemies/Bat.gd
@@ -1,16 +1,40 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+
+# STATE MACHINES
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
+
+var state = CHASE
 
 onready var stats = $Stats
 
-func _ready():
-	print(stats.max_health)
-	print(stats.health)
-
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200*delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		WANDER:
+			pass
+		CHASE:
+			pass
+			
+			
+func seek_player():
+	pass
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
@@ -19,3 +43,6 @@ func _on_Hurtbox_area_entered(area):
 
 func _on_Stats_no_health():
 	queue_free()
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
