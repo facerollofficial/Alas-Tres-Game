7ab7cb70aa283763001cd207f4c2dfede036a00e
rosemariecauilan <88758051+rosemariecauilan@users.noreply.github.com>
2022-09-27 15:02:51 +0800

    dialogue sala scene

---
+++ res/NPC/Lolo/Lolo.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+var active = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("body_entered",self, '_on_NPC_body_entered')
+	connect("body_exited",self, '_on_NPC_body_exited')
+
+#function for the mark if the player is inside the area the mark will show up
+func _process(delta):
+	$mark.visible = active
+
+#function for the dialogue 1; trigger_convo: use left key mouse to start convo
+func _input(event):
+	if get_node_or_null('DialogNode') == null:
+		if event.is_action_pressed("trigger_convo") and active:
+			get_tree().paused = true #allows the player not to move when convo is on going
+			var dialog = Dialogic.start('timeline-1')
+			dialog.pause_mode = Node.PAUSE_MODE_PROCESS 
+			dialog.connect('timeline_end',self,'unpause')
+			add_child(dialog)
+
+#unpause the player once conversation is done
+func unpause(timeline_name):
+	get_tree().paused = false
+
+#checks if the player is inside the area set for lolo or not; used for the mark if it will show up or not
+func _on_NPC_body_entered(body):
+	if body.name == 'Player':
+		active = true
+
+func _on_NPC_body_exited(body):
+	if body.name == 'Player':
+		active = false
