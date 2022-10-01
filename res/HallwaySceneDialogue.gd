d51d1ef586e124322927b8a833c2e731eaf297cb
rosemariecauilan <88758051+rosemariecauilan@users.noreply.github.com>
2022-10-01 22:09:12 +0800

    Player monologue

---
+++ res/HallwaySceneDialogue.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var active = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Player/mark.visible = false
+	connect("body_entered",self, 'area_entered')
+
+func area_entered(body):
+	if body.name == 'Player':
+		$Player/mark.visible = true
+		get_tree().paused = true
+		var dialog = Dialogic.start('hallwayMonologue')
+		dialog.pause_mode = Node.PAUSE_MODE_PROCESS
+		dialog.connect('timeline_end',self,'unpause')
+		add_child(dialog)
+
+#unpause function
+func unpause(timeline_name):
+	get_tree().paused = false
+	$Player/mark.visible = false
+	get_node("CollisionShape2D").disabled= true
---
+++ res/RoomSceneDialogue.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var active = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("body_entered",self,'area_entered')
+
+#function for the monologue
+func area_entered(body):
+	if body.name == 'Player':
+		get_tree().paused = true
+		var dialog = Dialogic.start('roomMonologue')
+		dialog.pause_mode = Node.PAUSE_MODE_PROCESS
+		dialog.connect('timeline_end',self,'unpause')
+		add_child(dialog)
+		
+#unpause function
+func unpause(timeline_name):
+	get_tree().paused = false
+	get_node("CollisionShape2D").disabled= true
