ed4fb5c2191489df4b398adc38972ed1612e9b7c
facerollofficial <82850357+facerollofficial@users.noreply.github.com>
2022-10-04 09:28:23 +0800

    rsc_22

--- res/UI/Intro/1/Intro1.gd
+++ res/UI/Intro/1/Intro1.gd
@@ -1,12 +1,18 @@
 extends Node2D
+var IntroIsPlayed = false
 
 func _ready():
 	
-	#$AnimationPlayer.play("SponsorIntro")
-	#yield(get_tree().create_timer(8),"timeout")
+	$AnimationPlayer.play("SponsorIntro")
+	yield(get_tree().create_timer(8),"timeout")
+	IntroIsPlayed = true
 	$AnimationPlayer.play("StoryIntro")
 	var new_dialog = Dialogic.start('StoryIntro')
 	add_child(new_dialog)
 	yield(get_tree().create_timer(59),"timeout")
-	
 	get_tree().change_scene("res://World.tscn")
+	
+func _process(delta):
+	if IntroIsPlayed:
+		if Input.is_action_just_pressed("ui_accept"):
+			get_tree().change_scene("res://World.tscn")
