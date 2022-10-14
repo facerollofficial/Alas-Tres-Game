8c621e457e74674b5c6d44472707b1ad1dd98a6d
facerollofficial <82850357+facerollofficial@users.noreply.github.com>
2022-10-14 10:40:17 +0800

    tikbalang v01

---
+++ res/Enemies/3_Tikbalang/AttackZone.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var player = null
+
+func target_within_attack_zone():
+	return player != null
+
+func _on_AttackZone_body_entered(body):
+	player = body
+
+
+func _on_AttackZone_body_exited(body):
+	player = null
--- res/Enemies/3_Tikbalang/Tikbalang.gd
+++ res/Enemies/3_Tikbalang/Tikbalang.gd
@@ -1,3 +1,31 @@
 extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/Combat/EnemyDeathEffect.tscn")
+
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+export var WANDER_TARGET_RANGE = 4
+
+var velocity = Vector2.ZERO
+var knockback = Vector2.ZERO
+
+var state = CHASE
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE,
+	ATTACK
+}
+
+## ONREADY VARIABLES ##
+onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
+onready var hurtbox = $Hurtbox
+onready var softCollision = $SoftCollision
+onready var wanderController = $WanderController
+onready var animationPlayer = $AnimationPlayer
+onready var blinkAnimationPlayer = $BlinkAnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
