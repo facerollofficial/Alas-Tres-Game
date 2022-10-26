87847d20bed68aa0286341ae617b7717777990f5
facerollofficial <82850357+facerollofficial@users.noreply.github.com>
2022-10-26 23:27:41 +0800

    GUI

--- res/UI/Title Screen/TitleScreen.gd
+++ res/UI/Title Screen/TitleScreen.gd
@@ -1,8 +1,22 @@
 extends Control
 
+onready var pauseMenu = $GUI/Pause
+
+func _ready():
+	pauseMenu.visible = false
+
 func _on_NewGame_pressed():
 	print(PlayerStats.max_health)
 	#play intro
 	get_tree().change_scene("res://UI/Intro/Intro Scripts/Intro2.tscn")
 	#transition to bus station
 	#get_tree().change_scene("res://Maps/Bus Station/BusStationScene.tscn")
+
+
+
+func _on_Options_pressed():
+	pauseMenu.visible = true
+
+
+func _on_Back_pressed():
+	pauseMenu.visible = false
