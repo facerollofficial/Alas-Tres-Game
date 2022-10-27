759cd8f2cc7d219de5bf42eb1392b2d4ba7c15b5
facerollofficial <82850357+facerollofficial@users.noreply.github.com>
2022-10-27 21:22:49 +0800

    StatsGD

---
+++ res/Player/PlayerStats.gd
@@ -0,0 +1,28 @@
+extends Node
+
+#config:
+var music_bus = AudioServer.get_bus_index("Music")
+var music_volume = -18
+var sfx_volume = -18
+onready var set_health = 4
+
+export(int) var max_health = 1 setget set_max_health
+var health = set_health setget set_health
+
+signal no_health
+signal health_changed(value)
+signal max_health_changed(value)
+
+func set_max_health(value):
+	max_health = value
+	self.health = min(health, max_health)
+	emit_signal("max_health_changed", max_health)
+	
+func set_health(value):
+	health = value
+	emit_signal("health_changed", health)
+	if health <= 0:
+		emit_signal("no_health")
+		
+func _ready():
+	self.health = max_health
