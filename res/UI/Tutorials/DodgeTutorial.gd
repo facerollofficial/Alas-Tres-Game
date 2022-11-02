cc7957b4d100c724121101773b2ab6f4cc4d253f
facerollofficial <82850357+facerollofficial@users.noreply.github.com>
2022-11-02 19:59:28 +0800

    fix: optimized dodge tutorial

--- res/UI/Tutorials/DodgeTutorial.gd
+++ res/UI/Tutorials/DodgeTutorial.gd
@@ -1,8 +1,6 @@
 extends Control
 
 onready var animation = $AnimationPlayer
-onready var lightanimation = $LightFlicker
 
 func _ready():
 	animation.play("SkipButton")
-	lightanimation.play("flickerlight")
