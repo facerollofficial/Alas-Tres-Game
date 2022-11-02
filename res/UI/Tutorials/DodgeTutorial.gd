5bce67404c6d8dba9d843e25170c2d48caa57add
facerollofficial <82850357+facerollofficial@users.noreply.github.com>
2022-11-02 19:55:00 +0800

    feat: added dodge tutorial

---
+++ res/UI/Tutorials/DodgeTutorial.gd
@@ -0,0 +1,8 @@
+extends Control
+
+onready var animation = $AnimationPlayer
+onready var lightanimation = $LightFlicker
+
+func _ready():
+	animation.play("SkipButton")
+	lightanimation.play("flickerlight")
