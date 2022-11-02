897a028d3ebd6eaf7f470fc87a060b0e81d98014
rosemariecauilan <88758051+rosemariecauilan@users.noreply.github.com>
2022-11-03 00:31:53 +0800

    Update Intro1.gd

--- res/UI/Intro/Intro Scripts/Intro1.gd
+++ res/UI/Intro/Intro Scripts/Intro1.gd
@@ -1,5 +1,5 @@
 extends Node2D
-var IntroIsPlayed = false
+#var IntroIsPlayed = false
 
 var music_bus = AudioServer.get_bus_index("Music")
 var sfx_bus = AudioServer.get_bus_index("SFX")
@@ -10,18 +10,18 @@ func _ready():
 	
 	$AnimationPlayer.play("SponsorIntro")
 	yield(get_tree().create_timer(8),"timeout")
-	IntroIsPlayed = true
-	$AnimationPlayer.play("StoryIntro")
-	var new_dialog = Dialogic.start('StoryIntro')
-	add_child(new_dialog)
-	yield(get_tree().create_timer(59),"timeout")
+#	IntroIsPlayed = true
+#	$AnimationPlayer.play("StoryIntro")
+#	var new_dialog = Dialogic.start('StoryIntro')
+#	add_child(new_dialog)
+#	yield(get_tree().create_timer(59),"timeout")
 	next_scene()
 	
-func _process(delta):
-	if IntroIsPlayed:
-		if Input.is_action_just_pressed("ui_accept"):
-			queue_free()
-			next_scene()
+#func _process(delta):
+#	if IntroIsPlayed:
+#		if Input.is_action_just_pressed("ui_accept"):
+#			queue_free()
+#			next_scene()
 
 func next_scene():
 	get_tree().change_scene("res://UI/Title Screen/TitleScreen.tscn")
