b6b755e1dda39d34f857f2455d6bb97a3628ee42
facerollofficial <82850357+facerollofficial@users.noreply.github.com>
2022-11-04 16:33:56 +0800

    feat: new sounds

---
+++ res/Audio/Autoload/MusicController.gd
@@ -0,0 +1,21 @@
+extends Node
+var normalCombatMusicIsPlaying = false
+var bossCombatMusicIsPlaying = false
+
+func _ready():
+	$HealSFX.play()
+
+func play_normal_combat():
+	$NormalCombat.play()
+
+func play_boss_combat():
+	$BossCombat.play()
+
+func pause_normal_combat():
+	$BossCombat.play()
+
+func play_heal_sfx():
+	$HealSFX.play()
+
+func play_level1_bgm():
+	$Level1BGM.play()
