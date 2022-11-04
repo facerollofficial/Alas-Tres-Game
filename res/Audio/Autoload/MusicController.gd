548933db4da1c14812623bfd6b9a6f80c9be6cd0
facerollofficial <82850357+facerollofficial@users.noreply.github.com>
2022-11-04 17:42:23 +0800

    fix: music

--- res/Audio/Autoload/MusicController.gd
+++ res/Audio/Autoload/MusicController.gd
@@ -3,22 +3,29 @@ var normalCombatMusicIsPlaying = false
 var bossCombatMusicIsPlaying = false
 
 func _ready():
-	$HealSFX.play()
+	pass
 
+# NORMAL COMAT
 func play_normal_combat():
-	$NormalCombat.play()
-
-func play_boss_combat():
-	$BossCombat.play()
+	$NormalCombat.playing = true
 
 func pause_normal_combat():
-	$BossCombat.play()
+	$BossCombat.stream_paused = true
 
+# BOSS COMBAT
+func play_boss_combat():
+	$BossCombat.playing = true
+
+func pause_boss_combat():
+	$BossCombat.stream_paused = true
+
+# SFX
 func play_heal_sfx():
 	$HealSFX.play()
 
+# BGMS
 func play_level1_bgm():
-	$Level1BGM.play()
+	$Level1BGM.playing = true
 
 func play_dark_ambience():
-	$DarkAmbience.play()
+	$DarkAmbience.playing = true
