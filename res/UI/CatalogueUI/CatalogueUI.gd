c213046593a1cd528b9860e9f6bc5afbbb9212b5
facerollofficial <82850357+facerollofficial@users.noreply.github.com>
2022-11-25 11:35:43 +0800

    catalogue

---
+++ res/UI/CatalogueUI/CatalogueUI.gd
@@ -0,0 +1,29 @@
+extends Control
+
+func _ready():
+	self.visible = false
+
+
+
+func _on_AghoyButton_pressed():
+	print("Aghoy displayed.")
+
+
+func _on_MangkukulamButton_pressed():
+	print("Mangkukulam displayed.")
+
+
+func _on_TiyanakButton_pressed():
+	print("Tiyanak displayed.")
+
+
+func _on_TikbalangButton_pressed():
+	print("Tikbalang displayed.")
+
+
+func _on_ManananggalButton_pressed():
+	print("Manananggal displayed.")
+
+
+func _on_BungisngisButton_pressed():
+	print("Bungisngis displayed.")
